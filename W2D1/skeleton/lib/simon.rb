16a8eec4a787cbfdf3a1e1d9bb86b1ec49d00c88
your name here <theo@dali.dartmouth.edu>
2018-10-26 21:01:35 -0700

    Add basic functionality for #Simon

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -1,22 +1,33 @@
-class Simon 
+class Simon
   COLORS = %w(red blue green yellow)
 
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over == true
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over == true
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +35,8 @@ class Simon
   end
 
   def add_random_color
-
+    colors = %w(red blue yellow green)
+    @seq.push(colors.sample)
   end
 
   def round_success_message
@@ -36,6 +48,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
   end
 end
