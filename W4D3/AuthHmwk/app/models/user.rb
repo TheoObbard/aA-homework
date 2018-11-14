97f35f333ba7e27295c1bb31c150f66638020c20
your name here <theo@dali.dartmouth.edu>
2018-11-13 20:34:55 -0800

    Complete methods for setting session token and encrypting PW

--- W4D3/AuthHmwk/app/models/user.rb
+++ W4D3/AuthHmwk/app/models/user.rb
@@ -12,33 +12,35 @@
 
 class User < ApplicationRecord
 
+  attr_reader :password
+
   validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: {message: "Password can't be blank"}
   validates :password, length: {minimum: 6, allow_nil: true}
   before_validation :ensure_session_token
 
-
-  def self.find_by_credentials
-    #This method takes in a username and a password and returns the user that matches
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.create(user.password_digest).is_password?(password)
+    return nil
   end
 
   def self.generate_session_token
-    # uses SecureRandom::urlsafe_base64 and returns a 16-digit pseudorandom string
+    session_token = SecureRandom::urlsafe_base64
   end
 
   def reset_session_token!
-    #This method resets the user's session_token and saves the user
+    @session_token = User.generate_session_token
+    self.save!
   end
 
   def ensure_session_token
-    # This method makes sure that the user has a session_token set, setting one if none exists
+    @session_token ||= reset_session_token
   end
 
-  def password=
-    #This method sets @password equal to the argument given so that the appropriate validation can happen
-    #This method also encrypts the argument given and saves it as this user's password_digest
-    #Remember: you have to add an attr_reader for password for the validation to occur!
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
-
 
 end
