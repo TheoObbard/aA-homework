7c3be553afbe4773d710cf265181e1f9da13d5b5
your name here <theo@dali.dartmouth.edu>
2018-11-13 20:20:07 -0800

    Create skeleton for pw and session cookie methods

--- W4D3/AuthHmwk/app/models/user.rb
+++ W4D3/AuthHmwk/app/models/user.rb
@@ -18,4 +18,27 @@ class User < ApplicationRecord
   before_validation :ensure_session_token
 
 
+  def self.find_by_credentials
+    #This method takes in a username and a password and returns the user that matches
+  end
+
+  def self.generate_session_token
+    # uses SecureRandom::urlsafe_base64 and returns a 16-digit pseudorandom string
+  end
+
+  def reset_session_token!
+    #This method resets the user's session_token and saves the user
+  end
+
+  def ensure_session_token
+    # This method makes sure that the user has a session_token set, setting one if none exists
+  end
+
+  def password=
+    #This method sets @password equal to the argument given so that the appropriate validation can happen
+    #This method also encrypts the argument given and saves it as this user's password_digest
+    #Remember: you have to add an attr_reader for password for the validation to occur!
+  end
+
+
 end
