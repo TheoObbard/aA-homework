fcbb671f33f392bd0d339ee673eaa3b3f787cd8c
your name here <theo@dali.dartmouth.edu>
2018-11-21 20:25:58 -0800

    Complete phase 2 of HMWK on callbacks

--- W5D4/hmwk_warmup.js
+++ W5D4/hmwk_warmup.js
@@ -62,3 +62,60 @@ function sumOfNPrimes(number) {
 }
 
 // console.log(sumOfNPrimes(1000));
+
+function formalize(name) {
+  return `Mx. ${name} the malevolent`
+}
+
+function titleize(names, callback) {
+  for (var i = 0; i < names.length; i++) {
+    console.log(callback(names[i]));
+  }
+}
+
+// titleize(['Theo', 'Drew', 'Bruce'], formalize);
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  return `${this.name} the Elephant goes 'phrRRRRRRRRRRR!!!!!!!'`
+};
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick)
+};
+
+Elephant.prototype.play = function () {
+  return `${this.name} is ${this.tricks[Math.floor(Math.random() * this.tricks.length)]}`
+};
+
+Elephant.prototype.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is trotting by!!!!`)
+};
+
+let micky = new Elephant('Micky', 154, ['chasing cars', 'juggling', 'cutting spinach'])
+
+// console.log(micky.play());
+// console.log(micky.trumpet());
+// console.log(micky.height);
+// console.log('growing...');
+// console.log(micky.grow());
+// console.log(micky.height);
+// console.log(micky.play());
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+herd.forEach(Elephant.prototype.paradeHelper);
