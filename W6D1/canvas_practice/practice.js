898ffd8fa32e469c0a90d91d7250a5d767408d0a
your name here <theo@dali.dartmouth.edu>
2018-11-25 14:06:27 -0800

    Set up canvas

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+  let hmm = document.getElementById('mycanvas');
+  hmm.height = 500;
+  hmm.width = 500;
+  ctx = hmm.getContext('2d')
 });
