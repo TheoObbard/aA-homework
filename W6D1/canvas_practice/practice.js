91daa5859eb11d455ccbee967379d65eee364765
your name here <theo@dali.dartmouth.edu>
2018-11-25 14:14:03 -0800

    create circle

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", function(){
   hmm.height = 500;
   hmm.width = 500;
   ctx = hmm.getContext('2d')
+
+  ctx.fillStyle = 'red'
+  ctx.fillRect(10, 100, 10, 10);
+
+  ctx.beginPath();
+  ctx.arc(350, 300, 100, 0, Math.PI * 2);
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 5;
+  ctx.stroke();
 });
