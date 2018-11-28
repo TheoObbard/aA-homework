f1d890b37e122f21158a3942db73e6fe6421fa73
your name here <theo@dali.dartmouth.edu>
2018-11-28 08:04:37 -0800

    Implement basic weather api to observe the running order of ajax

--- W6D3/skeleton-2/js/index.js
+++ W6D3/skeleton-2/js/index.js
@@ -2,6 +2,15 @@ console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
   $.ajax({
-
+    type: 'GET',
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    success(data) {
+      console.log("this is on success: ")
+      console.log(data);
+    },
+    error() {
+      console.log("an error occured");
+    },
   });
 // Add another console log here, outside your AJAX request
+console.log("Completed ajax request");
