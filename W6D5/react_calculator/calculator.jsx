04a23659c6d71739572f8d2d55446e7d0396aef3
your name here <theo@dali.dartmouth.edu>
2018-11-29 20:40:11 -0800

    Get react calculator working with summing

--- W6D5/react_calculator/calculator.jsx
+++ W6D5/react_calculator/calculator.jsx
@@ -4,22 +4,34 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {num1: "", num2: "", result: 0};
-    this.calculate = this.calculate.bind(this);
+    this.sum = this.sum.bind(this);
+    this.changeInput1 = this.changeInput1.bind(this);
+    this.changeInput2 = this.changeInput2.bind(this);
   }
 
-  changeInput() {
-    // handles logic of input change
+  changeInput1(e) {
+    this.setState({num1: e.target.value})
   }
 
-  calculate() {
-    this.state.result = 
+  changeInput2(e) {
+    this.setState({num2: e.target.value})
+  }
+
+  sum() {
+    let num1 = this.state.num1;
+    let num2 = this.state.num2
+    let ans = parseInt(num1) + parseInt(num2)
+    // console.log(num1, num2)
+    this.setState({result: ans})
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.result}</h1>
-        <button className="button" onClick={this.calculate}>
+        <input type='text' onChange={this.changeInput1}></input>
+        <input type='text' onChange={this.changeInput2}></input>
+        <button className="button" onClick={this.sum}>
           HIT DA BUTTON
         </button>
       </div>
