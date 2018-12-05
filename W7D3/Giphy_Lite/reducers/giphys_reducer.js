21a887f71491ea28110a3e2e5eb486c61165ec52
your name here <theo@dali.dartmouth.edu>
2018-12-04 20:17:59 -0800

    Set up giphy API and giphys reducer

--- W7D3/Giphy_Lite/reducers/giphys_reducer.js
+++ W7D3/Giphy_Lite/reducers/giphys_reducer.js
@@ -1,1 +1,17 @@
 import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
+
+const initialState = []
+
+const giphysReducer = (state = initialState, action) => {
+  var resp = {};
+  switch (action.type) {
+    case RECEIVE_SEARCH_GIPHYS:
+      resp = Object.assign({}, state);
+      resp.giphys = action.giphys;
+      return resp;
+    default:
+      return state;
+  }
+};
+
+export default giphysReducer;
